9c81f23a01b8b239e631abef3f89217663824313
SantiagoFeli <sfeliciani@correo.unimet.edu.ve>
2024-11-21 22:40:32 -0400

    ajaaaaaaaaa

---
+++ src/soproyecto2/main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soproyecto2;
+
+/**
+ *
+ * @author santi
+ */
+import Interfaces.InterfazPrincipal;
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        InterfazPrincipal IP = new InterfazPrincipal();
+        IP.setVisible(true);
+    }
+    
+}
